caec1a62b56f122ae56a50108fcbd776dd670a38
nagihanshnr <nagihanshnr@gmail.com>
2025-05-25 14:52:39 +0300

    Update app.py

--- ziyaretci_takip_sistemi/app.py
+++ ziyaretci_takip_sistemi/app.py
@@ -205,5 +205,5 @@ def user_delete(user_id):
     return redirect(url_for('users'))  # users sayfasına yönlendir
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", part=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
  
