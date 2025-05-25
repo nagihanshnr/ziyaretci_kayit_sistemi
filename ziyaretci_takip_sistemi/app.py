00a214e5b7062ef2461ed03e52cf8574edd22620
nagihanshnr <nagihanshnr@gmail.com>
2025-05-25 14:45:26 +0300

    Update app.py

--- ziyaretci_takip_sistemi/app.py
+++ ziyaretci_takip_sistemi/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-
+import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'gizli_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # SQLite kullanılıyor
@@ -202,14 +202,7 @@ def user_delete(user_id):
         db.session.rollback()
         flash('Kullanıcı silinirken bir hata oluştu.', 'danger')
     return redirect(url_for('users'))  # users sayfasına yönlendir
-
-
-#if __name__ == '__main__':
- #   with app.app_context():
-  #      db.create_all()
-   # app.run(debug=True)
-
-import os
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", part=int(os.environ.get("PORT", 5000)))
  
